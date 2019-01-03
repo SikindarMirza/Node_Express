ced049655b867ca621463f34bfb6846fb2aab316
SikindarMirza <sikindar247@gmail.com>
2019-01-03 17:39:21 +0530

    Sikindar | Used third party module lion-lib

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const _ = require('underscore');
 var result= _.contains([1,2,3], 3);
 
 console.log("check", result);
+
+var lion = require('lion-lib');
+
+var add = lion.add(1,3);
+var mult = lion.multiply(2,4);
+console.log("add",add,mult);
