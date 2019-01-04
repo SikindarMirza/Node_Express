c97565613957ec09796f27462d64279e642ee955
SikindarMirza <sikindar247@gmail.com>
2019-01-04 23:07:12 +0530

    Sikindar | Post request for the course

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id:1, name: "course1" },
     { id:2, name: "course2" },
@@ -9,17 +11,26 @@ const courses = [
 
 app.get('/',(req,res)=>{
     res.send("Hello world!!!");
-})
+});
 
 app.get('/api/courses', (req,res) => {
     res.send(courses);
-})
+});
 
 app.get('/api/courses/:id', (req, res) => {
     const course =courses.find( c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send("Course not found");
     res.send(course);
-})
+});
 
-const port = process.env.PORT || 3003;
+app.post('api/courses', (req,res) => {
+    const course = {
+        id : courses.length+1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log(`Listning on the port ${port}...`)});
