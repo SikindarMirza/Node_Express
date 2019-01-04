2ac76481b69f02238c0c38c2625c5d0062c1a798
SikindarMirza <sikindar247@gmail.com>
2019-01-05 01:45:54 +0530

    Adds Joi for input validation for post method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
@@ -23,11 +24,20 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
-app.post('api/courses', (req,res) => {
-    const course = {
-        id : courses.length+1,
-        name: req.body.name
+
+app.post('/api/courses', (req,res) => {
+    const schema = {
+        name : Joi.string().min(3).required()
     }
+    const result = Joi.validate(req.body, schema);
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id : courses.length + 1,
+        name: req.body.name
+    };
     courses.push(course);
     res.send(course);
 });
