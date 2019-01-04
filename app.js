8357071ecf672db4a5ad1db48cdc0e3312c24c70
SikindarMirza <sikindar247@gmail.com>
2019-01-05 02:32:35 +0530

    Delete request

--- app.js
+++ app.js
@@ -59,6 +59,18 @@ app.put('/api/courses/:id', (req,res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req,res) => {
+    
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("The course with the given id is not present.");
+    
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+});
+
 function validateInputs(course) {
     const schema = {
         name: Joi.string().min(3).required()
