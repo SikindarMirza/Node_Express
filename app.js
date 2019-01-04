8854f472e2067797c885b03713708e7db1a2038c
SikindarMirza <sikindar247@gmail.com>
2019-01-04 17:04:23 +0530

    Sikindar | Added a route and course data

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3003;
+
+const courses = [
+    { id:1, name: "course1" },
+    { id:2, name: "course2" },
+    { id:3, name: "course3" }    
+]
+
 app.get('/',(req,res)=>{
     res.send("Hello world!!!");
 })
 
-app.get('/api/cources', (req,res) => {
-    res.send([1,2,3]);
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
 })
 
-app.listen(port, ()=>{console.log(`Listning on port ${port}...`)});
+app.get('/api/courses/:id', (req, res) => {
+    const course =courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("Course not found");
+    res.send(course);
+})
+
+const port = process.env.PORT || 3003;
+app.listen(port, ()=>{console.log(`Listning on the port ${port}...`)});
