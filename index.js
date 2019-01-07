6230eb14d18690b81fd26a2bf704542cc99b6b7b
SikindarMirza <sikindar247@gmail.com>
2019-01-08 01:11:55 +0530

    Callbacks for with named functions

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
-console.log("Hello");
-getUser(1, getRepo);
+// console.log("Hello");
+// getUser(1, getRepo);
 
-function displayCommits(commits) {
-    console.log(commits);
-}
-function getCommits(repos) {
-    getCommits(repo, displayCommits);
-}
-function getRepo(user){
-    getRepository(user.gitHubUserName, getCommits);
-}
+// function displayCommits(commits) {
+//     console.log(commits);
+// }
+// function getCommits(repos) {
+//     // getCommits(repos, displayCommits);
+// }
+// function getRepo(user){
+//     getRepository(user.gitHubUserName, getCommits);
+// }
+// console.log("World");
+
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         console.log("Reading a user from the database");
+//         callback({id: id, gitHubUserName: "SikindarMirza"});
+//     },2000);
+// };
+
+// function getRepository(username, callback){
+//     setTimeout(() => {
+//         console.log("Calling the github API...");
+//         callback(['repo1', 'repo2', 'repo3']);
+//     }, 2000);
+// }
+
+console.log("Hello");
+
+getUser(1, repositories);
+
 console.log("World");
+
+function commits(commits) {
+    console.log(`Commits are ${commits}`)
+}
+
+function pullrequests(pullRequests) {
+    console.log(`All pullRequests are ${pullRequests}`);
+    getCommits(pullRequests[0], commits)
+}
+
+function repos(repos) {
+    getPullRequests(repos, pullrequests);
+}
+
+function repositories(user) {
+    getRepositories(user.gitHubUserName, repos);
+}
 
 function getUser(id, callback) {
     setTimeout(() => {
-        console.log("Reading a user from the database");
-        callback({id: id, gitHubUserName: "SikindarMirza"});
-        
+        console.log("Reading data from the database...");
+        callback({ id:id, gitHubUserName:'SikindarMirza' });
     },2000);
-};
+}
 
-function getRepository(username, callback){
+function getRepositories(user, callback) {
+    setTimeout(()=>{
+        console.log("Getting the repos of", user);
+        callback(["repo1", "repo2", "repo3"]);
+    },3000);
+}
+
+function getPullRequests(repos, callback) {
     setTimeout(() => {
-        console.log("Calling the github API...");
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+        callback(['pullRequest1', 'pullRequest2', 'pullRequest3']);
+    },2000);
+    console.log(`The pullRequests of ${repos[0]} are`);
+}
+
+function getCommits(pullrequest, callback) {
+    setTimeout(() => {
+        callback(['Commit1','Commit2','Commit3']);
+    },2000);
 }
